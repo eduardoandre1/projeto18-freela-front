a92666c774913bc6f1f65f0adea719fd781d13d5
Eduardo André <eduardo_andre@id.uff.br>
2023-08-10 23:20:30 -0300

    feat : working

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { BrowserRouter,Routes,Route} from "react-router-dom"
 import Resetstyle from './reset'
 import Login1 from './pages/Loginpage'
 import ClientMaker from "./pages/cadastro_client"
+import { useState } from "react"
+import {UseContext} from "./createContext"
+import { useContext } from "react"
 function App() {
+  const [token,setToken] = useState('')
+  const config = {
+    headers: {
+      "Authorization": token
+    }
+  }
   return (    
     <>
-        <Resetstyle />
+      <UseContext.Provider value={{config,token,setToken}}>
+      <Resetstyle />
         <BrowserRouter>
           <Routes>        
             <Route path='/'  element={<Login1 />}/>
             <Route path ="/cadastro" element={<ClientMaker />} />
           </Routes>
         </BrowserRouter>
+      </UseContext.Provider>
     </>
   )
 }
